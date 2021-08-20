54a9348833cc6f6366dc647d46436a32437131dc
hyuk <pjh08190819@naver.com>
2021-08-20 23:52:46 +0900

    study getmapping

--- src/main/java/com/example/demo/controller/GetApiController.java
+++ src/main/java/com/example/demo/controller/GetApiController.java
@@ -1,15 +1,47 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
 public class GetApiController {
 
-    @GetMapping("/hello")   // http://localhost:9090/api/get/hello
-    public String getHello() {
+    // http://localhost:9090/api/get/hello
+    @GetMapping("/hello")
+    public String hello() {
         return "get hello";
+    }
+
+    // http://localhost:9090/api/get/hi
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)  // 예전 방식
+    public String hi() {
+        return "hi";
+    }
+
+    // http://localhost:9090/api/get/path-variable/{}
+    @GetMapping("/path-varible/{id}")
+    public String parthVariable(@PathVariable(name = "id") String pathName) {
+        System.out.println("PathVariable : " + pathName);
+
+        return pathName;
+    }
+
+    // http://localhost:9090/api/get/query-param?user=pjh0819&email=pjh0819@naver.com&age=29
+    @GetMapping("query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam) {
+
+        StringBuilder sb = new StringBuilder();
+
+        queryParam.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+
+            sb.append(entry.getKey() + " = " + entry.getValue() + "\n");
+        });
+
+        return sb.toString();
     }
 }

46c7a79aae1d0cf6405dfb977cd139a1f3e36956
hyuk <pjh08190819@naver.com>
2021-09-04 02:06:16 +0900

    Study validation

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Getter @Setter
@@ -14,10 +15,21 @@ import java.util.List;
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)    // null 제외
 public class User {
+
+    @NotBlank
     private String name;
+
+    @Max(value = 90)
     private Integer age;
+
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxxx-xxxx")
     private String phoneNumber;
+
+    @Email
+    private String email;
+
     private String address;
+
     private List<Car> cars;
 
     @Override

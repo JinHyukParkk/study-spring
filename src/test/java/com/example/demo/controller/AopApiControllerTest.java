c8022c0f635b5d2ef8959ccf134887879463f4ce
hyuk <pjh08190819@naver.com>
2021-09-18 23:50:26 +0900

    study mock

--- src/test/java/com/example/demo/controller/AopApiControllerTest.java
+++ src/test/java/com/example/demo/controller/AopApiControllerTest.java
@@ -1,7 +1,26 @@
 package com.example.demo.controller;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled
+@WebMvcTest(controllers = AopApiController.class)
 class AopApiControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void testTriggerException() throws Exception {
+        mockMvc.perform(get("/oups")).andExpect(view().name("exception"))
+                .andExpect(model().attributeExists("exception")).andExpect(forwardedUrl("exception"))
+                .andExpect(status().isOk());
+    }
 }

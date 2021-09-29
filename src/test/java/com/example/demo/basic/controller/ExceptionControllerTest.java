e46a63de1f63a38d0b5552e6c126594982d7dc8e
hyuk <pjh08190819@naver.com>
2021-09-29 23:47:48 +0900

    study server to server

--- src/test/java/com/example/demo/basic/controller/ExceptionControllerTest.java
+++ src/test/java/com/example/demo/basic/controller/ExceptionControllerTest.java
@@ -1,5 +1,20 @@
 package com.example.demo.basic.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = ExceptionController.class)
 class ExceptionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void test() {
+
+    }
 
 }

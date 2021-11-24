2c7a2f18ff42c2dd360714fcdfbdea5e4e13a876
hyuk <pjh08190819@naver.com>
2021-11-24 20:54:47 +0900

    add test given

--- jpa-study/src/test/java/com/example/jpastudy/tmp/repository/LockerRepositoryTest.java
+++ jpa-study/src/test/java/com/example/jpastudy/tmp/repository/LockerRepositoryTest.java
@@ -1,8 +1,12 @@
 package com.example.jpastudy.tmp.repository;
 
+import com.example.jpastudy.tmp.domain.Locker;
+import com.example.jpastudy.tmp.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.locks.Lock;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +19,40 @@ class LockerRepositoryTest {
     @Test
     void crudTest() {
 
+    }
+
+    private Member givenMember(String nickName, String email) {
+        Member member = Member.builder()
+                .nickName(nickName)
+                .email(email)
+                .build();
+
+        return member;
+    }
+
+    private Member givenMember(String nickName, String email, Locker locker) {
+        Member member = Member.builder()
+                .nickName(nickName)
+                .email(email)
+                .locker(locker)
+                .build();
+
+        return member;
+    }
+
+    private Locker givenLocker(String name) {
+        Locker locker = new Locker().builder()
+                .name(name)
+                .build();
+
+        return locker;
+    }
+    private Locker givenLocker(String name, Member member) {
+        Locker locker = new Locker().builder()
+                .name(name)
+                .member(member)
+                .build();
+
+        return locker;
     }
 }

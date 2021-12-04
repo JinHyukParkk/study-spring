58816749e2fbddc2b3561e1e0d432d3acf07c72f
hyuk <pjh08190819@naver.com>
2021-12-05 03:54:17 +0900

    :white_check_mark: remove import

--- jpa-study/src/main/java/com/example/jpastudy/book/domain/Book.java
+++ jpa-study/src/main/java/com/example/jpastudy/book/domain/Book.java
@@ -4,9 +4,15 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.http.HttpStatus;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;

972cecba8f903428aab7bd3e7cfb5ad3b0e3e7c8
hyuk <pjh08190819@naver.com>
2022-03-23 16:32:57 +0900

    study : task, chunk 동작 비교

--- spring-batch-basic/src/main/java/com/example/batch/part3/ChunkProcessingConfiguration.java
+++ spring-batch-basic/src/main/java/com/example/batch/part3/ChunkProcessingConfiguration.java
@@ -3,10 +3,15 @@ package com.example.batch.part3;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,7 +37,31 @@ public class ChunkProcessingConfiguration {
         return jobBuilderFactory.get("chunkProcessingJob")
                 .incrementer(new RunIdIncrementer())
                 .start(this.taskBaseStep())
+                .next(this.chunkBaseStep())
                 .build();
+    }
+
+    @Bean
+    public Step chunkBaseStep() {
+        return stepBuilderFactory.get("chunkBaseStep")
+                .<String, String>chunk(10)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemWriter<String> itemWriter() {
+        return items -> log.info("chunk item size : {}", items.size());
+//        return items -> items.forEach(log::info);
+    }
+
+    private ItemProcessor<String, String> itemProcessor() {
+        return item -> item + ", Spring Batch";
+    }
+
+    private ItemReader<String> itemReader() {
+        return new ListItemReader<>(getItems());
     }
 
     @Bean
@@ -43,11 +72,25 @@ public class ChunkProcessingConfiguration {
     }
 
     private Tasklet tasklet() {
-        return (contribution, chunkContext) -> {
-            List<String> items = getItems();
-            log.info("task item size : {}", items.size());
+        List<String> items = getItems();
 
-            return RepeatStatus.FINISHED;
+        return (contribution, chunkContext) -> {
+            StepExecution stepExecution = contribution.getStepExecution();
+
+            int chunkSize = 10;
+            int fromIndex = stepExecution.getReadCount();
+            int toIndex = fromIndex + chunkSize;
+
+            if (fromIndex >= items.size()) {
+                return RepeatStatus.FINISHED;
+            }
+
+            List<String> subList = items.subList(fromIndex, toIndex);
+
+            stepExecution.setReadCount(toIndex);
+            log.info("task item size : {}", subList.size());
+
+            return RepeatStatus.CONTINUABLE;
         };
     }
 

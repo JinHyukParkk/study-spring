d592b4c194199ccc20dcd96c0dda4977425a889c
hyuk <pjh08190819@naver.com>
2022-03-30 02:06:52 +0900

    study : itemReader 사용

---
+++ spring-batch-basic/src/main/java/com/example/batch/part3/domain/Person.java
@@ -0,0 +1,25 @@
+package com.example.batch.part3.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Person {
+
+    private int id;
+
+    private String name;
+
+    private String age;
+
+    private String address;
+
+    public Person(int id,
+                  String name,
+                  String age,
+                  String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+}

923ec061b51360f1283f27ac733f3f25b2d10a8b
hyuk <pjh08190819@naver.com>
2022-04-11 02:47:31 +0900

    study : 간단 batch 만들어보기

---
+++ spring-batch-basic/src/main/java/com/example/batch/part3/misson/DuplicateValidationProcessor.java
@@ -0,0 +1,40 @@
+package com.example.batch.part3.misson;
+
+
+
+import org.springframework.batch.item.ItemProcessor;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+public class DuplicateValidationProcessor<T> implements ItemProcessor<T, T> {
+
+    private final Map<String, Object> keyPool = new ConcurrentHashMap<>();
+    private final Function<T, String> keyExtractor;
+    private final boolean allowDuplicate;
+
+
+    public DuplicateValidationProcessor(Function<T, String> keyExtractor,
+                                        boolean allowDuplicate) {
+        this.keyExtractor = keyExtractor;
+        this.allowDuplicate = allowDuplicate;
+    }
+
+    @Override
+    public T process(T item) throws Exception {
+        if (allowDuplicate) {
+            return item;
+        }
+
+        String key = keyExtractor.apply(item);
+
+        if (keyPool.containsKey(key)) {
+            return null;
+        }
+
+        keyPool.put(key, key);
+
+        return item;
+    }
+}
---
+++ spring-batch-basic/src/main/java/com/example/batch/part3/misson/SavePersonConfiguration.java
@@ -0,0 +1,104 @@
+package com.example.batch.part3.misson;
+
+import com.example.batch.part3.domain.Person;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.support.CompositeItemWriter;
+import org.springframework.batch.item.support.builder.CompositeItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@Slf4j
+public class SavePersonConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    public SavePersonConfiguration(JobBuilderFactory jobBuilderFactory,
+                                   StepBuilderFactory stepBuilderFactory,
+                                   EntityManagerFactory entityManagerFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Bean
+    public Job savePersonJob() throws Exception {
+        return this.jobBuilderFactory.get("savePersonJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.savePersonStep(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step savePersonStep(@Value("#{jobParameters[allow_duplicate]}") String allowDuplicate) throws Exception {
+        return this.stepBuilderFactory.get("savePersonStep")
+                .<Person, Person>chunk(10)
+                .reader(itemReader())
+                .processor(new DuplicateValidationProcessor<>(Person::getName, Boolean.parseBoolean(allowDuplicate)))
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemWriter<? super Person> itemWriter() throws Exception {
+        JpaItemWriter<Person> jpaItemWriter = new JpaItemWriterBuilder<Person>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+
+        ItemWriter<Person> logItemWriter = items -> log.info("person.size : {}", items.size());
+
+        CompositeItemWriter<Person> itemWriter = new CompositeItemWriterBuilder<Person>()
+                .delegates(jpaItemWriter, logItemWriter)
+                .build();
+
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
+//        return items -> items.forEach(item -> log.info(item.getName()));
+    }
+
+    private ItemReader<? extends Person> itemReader() throws Exception {
+        DefaultLineMapper<Person> defaultLineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setNames("name", "age", "address");
+        defaultLineMapper.setLineTokenizer(lineTokenizer);
+
+        defaultLineMapper.setFieldSetMapper(fieldSet -> new Person(
+                fieldSet.readString(0),
+                fieldSet.readString(1),
+                fieldSet.readString(2)
+        ));
+
+        FlatFileItemReader<Person> savePersonItemReader = new FlatFileItemReaderBuilder<Person>()
+                .name("savePersonItemReader")
+                .encoding("UTF-8")
+                .linesToSkip(1)
+                .resource(new ClassPathResource("person.csv"))
+                .lineMapper(defaultLineMapper)
+                .build();
+
+        savePersonItemReader.afterPropertiesSet();
+
+        return savePersonItemReader;
+    }
+}

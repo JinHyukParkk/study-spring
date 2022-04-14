469ac85abc14daa6038c1cbd9239af4e8ebe0e78
hyuk <pjh08190819@naver.com>
2022-04-14 23:39:23 +0900

    feat : 중간 테스트 코드 작성

--- spring-batch-basic/src/test/java/com/example/batch/part3/misson/SavePersonConfigurationTest.java
+++ spring-batch-basic/src/test/java/com/example/batch/part3/misson/SavePersonConfigurationTest.java
@@ -1,7 +1,29 @@
 package com.example.batch.part3.misson;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.batch.part3.TestConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {SavePersonConfiguration.class, TestConfiguration.class})
 class SavePersonConfigurationTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Test
+    public void test_allow_duplicate() {
+        // given
+        JobParameters jobParametersnew = new JobParametersBuilder()
+                .addString("allow_duplicate", "false")
+                .toJobParameters();
+
+    }
 
 }

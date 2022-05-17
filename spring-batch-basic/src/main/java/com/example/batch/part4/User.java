7e123a23d60288517ed73149eba8111590542b38
hyuk <pjh08190819@naver.com>
2022-05-17 23:58:36 +0900

    study : User 객체 세팅

--- spring-batch-basic/src/main/java/com/example/batch/part4/User.java
+++ spring-batch-basic/src/main/java/com/example/batch/part4/User.java
@@ -1,12 +1,16 @@
 package com.example.batch.part4;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Getter
 @Entity
@@ -16,4 +20,23 @@ public class User {
     @Id
     @GeneratedValue(strategy =GenerationType.IDENTITY)
     private Long id;
+
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private Level level = Level.NORMAL;
+
+    private int totalAmount;
+
+    private LocalDate updatedDate;
+
+    @Builder
+    private User(String username, int totalAmount) {
+        this.username = username;
+        this.totalAmount = totalAmount;
+    }
+
+    public enum Level {
+        VIP, GOLD, SILVER, NORMAL
+    }
 }

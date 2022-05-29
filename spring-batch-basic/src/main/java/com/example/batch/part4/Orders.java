f1b33a808ca0b3f69609217b40e06f6ff077388d
hyuk <pjh08190819@naver.com>
2022-05-29 23:50:13 +0900

    study : Order 엔티티 추가

---
+++ spring-batch-basic/src/main/java/com/example/batch/part4/Orders.java
@@ -0,0 +1,34 @@
+package com.example.batch.part4;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String itemName;
+
+    private int amount;
+
+    private LocalDate createdDate;
+
+    @Builder
+    public Orders(String itemName, int amount, LocalDate createdDate) {
+        this.itemName = itemName;
+        this.amount = amount;
+        this.createdDate = createdDate;
+    }
+}

dbdfa953a8707fa0f8eff81e515387d64c7fc17a
hyuk <pjh08190819@naver.com>
2022-05-31 01:00:10 +0900

    study : 합계 Step 추가

--- spring-batch-basic/src/main/java/com/example/batch/part4/UserConfiguration.java
+++ spring-batch-basic/src/main/java/com/example/batch/part4/UserConfiguration.java
@@ -1,38 +1,60 @@
 package com.example.batch.part4;
 
+import com.example.batch.part5.JobParametersDecide;
+import com.example.batch.part5.OrderStatistics;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @Slf4j
 public class UserConfiguration {
 
+    private final int CHUNK = 100;
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final UserRepository userRepository;
     private final EntityManagerFactory entityManagerFactory;
+    private final DataSource dataSource;
 
     public UserConfiguration(JobBuilderFactory jobBuilderFactory,
                              StepBuilderFactory stepBuilderFactory,
                              UserRepository userRepository,
-                             EntityManagerFactory entityManagerFactory) {
+                             EntityManagerFactory entityManagerFactory,
+                             DataSource dataSource) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
         this.userRepository = userRepository;
         this.entityManagerFactory = entityManagerFactory;
+        this.dataSource = dataSource;
     }
 
     @Bean
@@ -46,6 +68,72 @@ public class UserConfiguration {
     }
 
     @Bean
+    @JobScope
+    public Step orderStatisticStep(@Value("#{jobParameters[date]}") String date) throws Exception {
+        return this.stepBuilderFactory.get("orderStatisticsStep")
+                .<OrderStatistics, OrderStatistics>chunk(CHUNK)
+                .reader(orderStatisticsItemReader(date))
+                .writer(orderStatisticsItemWriter(date))
+                .build();
+    }
+
+    private ItemWriter<? super OrderStatistics> orderStatisticsItemWriter(String date) throws Exception {
+        YearMonth yearMonth = YearMonth.parse(date);
+
+        String fileName = yearMonth.getYear() + "년_" + yearMonth.getMonthValue() + "월_일별_주문_금액.csv";
+
+        BeanWrapperFieldExtractor<OrderStatistics> fieldExtractor = new BeanWrapperFieldExtractor<>();
+        fieldExtractor.setNames(new String[] {"amount", "date"});
+
+        DelimitedLineAggregator<OrderStatistics> lineAggregator = new DelimitedLineAggregator<>();
+        lineAggregator.setDelimiter(",");
+        lineAggregator.setFieldExtractor(fieldExtractor);
+
+        FlatFileItemWriter<OrderStatistics> itemWriter = new FlatFileItemWriterBuilder<OrderStatistics>()
+                .resource(new FileSystemResource("output/" + fileName))
+                .lineAggregator(lineAggregator)
+                .name("orderStatisticsItemWriter")
+                .encoding("UTF-8")
+                .headerCallback(writer -> writer.write("total_amount, date"))
+                .build();
+
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
+    }
+
+    private ItemReader<? extends OrderStatistics> orderStatisticsItemReader(String date) throws Exception {
+        YearMonth yearMonth = YearMonth.parse(date);
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("startDate", yearMonth.atDay(1));
+        parameters.put("endDate", yearMonth.atEndOfMonth());
+
+        Map<String, Order> sortKey = new HashMap<>();
+        sortKey.put("created_date", Order.ASCENDING);
+
+        JdbcPagingItemReader<OrderStatistics> itemReader = new JdbcPagingItemReaderBuilder<OrderStatistics>()
+                .dataSource(this.dataSource)
+                .rowMapper((resultSet, i) -> OrderStatistics.builder()
+                        .amount(resultSet.getString(1))
+                        .date(LocalDate.parse(resultSet.getString(2), DateTimeFormatter.ISO_DATE))
+                        .build())
+                .pageSize(CHUNK)
+                .name("orderStatisticsItemReader")
+                .selectClause("sum(amount), created_date")
+                .fromClause("orders")
+                .whereClause("created_date >= :startDate and created_date <= :endDate")
+                .groupClause("created_date")
+                .parameterValues(parameters)
+                .sortKeys(sortKey)
+                .build();
+
+        itemReader.afterPropertiesSet();
+
+        return itemReader;
+    }
+
+    @Bean
     public Step saveUserStep() {
         return this.stepBuilderFactory.get("saveUserStep")
                 .tasklet(new SaveUserTasklet(userRepository))
@@ -55,7 +143,7 @@ public class UserConfiguration {
     @Bean
     public Step userLevelUpStep() throws Exception {
         return this.stepBuilderFactory.get("userLevelUpStep")
-                .<User, User>chunk(100)
+                .<User, User>chunk(CHUNK)
                 .reader(itemReader())
                 .processor(itemProcessor())
                 .writer(itemWriter())
@@ -85,7 +173,7 @@ public class UserConfiguration {
         JpaPagingItemReader<User> itemReader = new JpaPagingItemReaderBuilder<User>()
                 .queryString("select u from User u")
                 .entityManagerFactory(entityManagerFactory)
-                .pageSize(100)  // chunk size 와 동일하게
+                .pageSize(CHUNK)  // chunk size 와 동일하게
                 .name("userItemReader")
                 .build();
 
--- spring-batch-basic/src/main/java/com/example/batch/part5/JobParametersDecide.java
+++ spring-batch-basic/src/main/java/com/example/batch/part5/JobParametersDecide.java
@@ -1,2 +1,30 @@
-package com.example.batch.part5;public class JobParametersDecide {
+package com.example.batch.part5;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.util.StringUtils;
+
+public class JobParametersDecide implements JobExecutionDecider {
+
+    public static final FlowExecutionStatus CONTINUE = new FlowExecutionStatus("CONTINUE");
+
+    private final String key;
+
+    public JobParametersDecide(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+        String value = jobExecution.getJobParameters().getString(key);
+
+        if (StringUtils.hasText(value)) {
+            return FlowExecutionStatus.COMPLETED;
+
+        }
+
+        return CONTINUE;
+    }
 }

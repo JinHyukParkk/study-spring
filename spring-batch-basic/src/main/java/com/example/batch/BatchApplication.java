8b0a433fe8ae856725a4357699d93abd63317979
hyuk <pjh08190819@naver.com>
2022-06-04 23:59:05 +0900

    study : async 사용을 위한 기본 세팅

--- spring-batch-basic/src/main/java/com/example/batch/BatchApplication.java
+++ spring-batch-basic/src/main/java/com/example/batch/BatchApplication.java
@@ -3,13 +3,26 @@ package com.example.batch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableBatchProcessing
 public class BatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BatchApplication.class, args);
+        System.exit(
+                SpringApplication.exit(
+                        SpringApplication.run(BatchApplication.class, args)));
+    }
+
+    @Bean
+    @Primary
+    TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        return taskExecutor;
     }
 
 }

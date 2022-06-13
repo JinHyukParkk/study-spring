0070e6cdebdca10b96f765e3f5a2da80a0ee9bd1
hyuk <pjh08190819@naver.com>
2022-06-13 23:55:40 +0900

    feat : 테스트 데이터 추가

---
+++ practice-batch/src/main/java/com/example/practicebatch/batch/tasklet/user/SaveUserTasklet.java
@@ -0,0 +1,63 @@
+package com.example.practicebatch.batch.tasklet.user;
+
+import com.example.practicebatch.domain.repository.UserRepository;
+import com.example.practicebatch.domain.user.User;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SaveUserTasklet implements Tasklet {
+
+    private final int SIZE = 100;
+    private final UserRepository userRepository;
+
+    public SaveUserTasklet(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        List<User> users = createUsers();
+
+        Collections.shuffle(users);
+
+        userRepository.saveAll(users);
+
+        return RepeatStatus.FINISHED;
+    }
+
+    private List<User> createUsers() {
+        List<User> users = new ArrayList<>();
+
+        for (int i = 0; i < SIZE; i++) {
+            users.add(User.builder()
+                    .username("test username" + i)
+                    .build());
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            users.add(User.builder()
+                    .username("test username" + i)
+                    .build());
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            users.add(User.builder()
+                    .username("test username" + i)
+                    .build());
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            users.add(User.builder()
+                    .username("test username" + i)
+                    .build());
+        }
+
+        return users;
+    }
+}

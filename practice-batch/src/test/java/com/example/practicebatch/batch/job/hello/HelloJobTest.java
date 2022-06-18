22d6093168c3ddaca041e1b3948c53cd50998f2d
hyuk <pjh08190819@naver.com>
2022-06-18 23:44:13 +0900

    test : jobLauncherTestUtils 추가

--- practice-batch/src/test/java/com/example/practicebatch/batch/job/hello/HelloJobTest.java
+++ practice-batch/src/test/java/com/example/practicebatch/batch/job/hello/HelloJobTest.java
@@ -1,7 +1,9 @@
 package com.example.practicebatch.batch.job.hello;
 
 import org.junit.runner.RunWith;
+import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,5 +11,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {HelloJob.class})
 class HelloJobTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
 
 }
--- practice-batch/src/test/java/com/example/practicebatch/batch/step/hello/HelloStepTest.java
+++ practice-batch/src/test/java/com/example/practicebatch/batch/step/hello/HelloStepTest.java
@@ -1,7 +1,18 @@
 package com.example.practicebatch.batch.step.hello;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.runner.RunWith;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBatchTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {HelloStep.class})
 class HelloStepTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
 
 }

43394a6f1dc3793a7c81861539978d38c5d95e2d
hyuk <pjh08190819@naver.com>
2022-06-19 17:36:24 +0900

    test : SaveUser Job 정상 동작 테스트 코드 작성

--- practice-batch/src/test/java/com/example/practicebatch/batch/TestConfiguration.java
+++ practice-batch/src/test/java/com/example/practicebatch/batch/TestConfiguration.java
@@ -1,8 +1,10 @@
 package com.example.practicebatch.batch;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -15,4 +17,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class TestConfiguration {
 
+    @Bean
+    public JobLauncherTestUtils jobLauncherTestUtils() {
+        return new JobLauncherTestUtils();
+    }
 }
--- practice-batch/src/test/java/com/example/practicebatch/batch/job/hello/HelloJobTest.java
+++ practice-batch/src/test/java/com/example/practicebatch/batch/job/hello/HelloJobTest.java
@@ -1,18 +1,40 @@
 package com.example.practicebatch.batch.job.hello;
 
+import com.example.practicebatch.batch.TestConfiguration;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBatchTest
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {HelloJob.class})
+@ContextConfiguration(classes = {
+        HelloJob.class,
+        TestConfiguration.class})
 class HelloJobTest {
 
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
 
+    @Test
+    public void 정상작동하는지_확인한다() throws Exception {
+        // given
+        JobParameters jobParameters = new JobParametersBuilder()
+                .toJobParameters();
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+        // then
+        assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+    }
 }

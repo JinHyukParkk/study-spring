b6d30c5227751bf3546f557535ea37fecc7f9450
hyuk <pjh08190819@naver.com>
2022-06-20 23:43:17 +0900

    feat : SaveUserJob 구현

---
+++ practice-batch/src/main/java/com/example/practicebatch/batch/job/user/SaveUserJob.java
@@ -0,0 +1,27 @@
+package com.example.practicebatch.batch.job.user;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SaveUserJob {
+
+    private final String SERVICE_NAME = "user";
+    private final JobBuilderFactory jobBuilderFactory;
+    private final Step saveUserSettingStep;
+
+    public SaveUserJob(JobBuilderFactory jobBuilderFactory,
+                       Step saveUserSettingStep) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.saveUserSettingStep = saveUserSettingStep;
+    }
+
+    @Bean(SERVICE_NAME + "Job")
+    public Job userJob() {
+        return this.jobBuilderFactory.get(SERVICE_NAME + "Job")
+                .build();
+    }
+}

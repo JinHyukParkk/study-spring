ba0d66bbb50ceddcf2d65893b209d8b1dcef2077
hyuk <pjh08190819@naver.com>
2022-08-06 23:42:45 +0900

    init : receiver 프로젝트 초기 생성

---
+++ mini-receiver/src/main/java/com/example/minireceiver/MiniReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.example.minireceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniReceiverApplication.class, args);
+    }
+
+}

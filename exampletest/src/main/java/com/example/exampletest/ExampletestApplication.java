ca722ebf836fce214daaeeae176f8118c70fd9a7
hyuk <pjh08190819@naver.com>
2022-12-18 21:59:38 +0900

    study : PostContruct + AOP 테스트

--- exampletest/src/main/java/com/example/exampletest/ExampletestApplication.java
+++ exampletest/src/main/java/com/example/exampletest/ExampletestApplication.java
@@ -2,8 +2,10 @@ package com.example.exampletest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class ExampletestApplication {
 
     public static void main(String[] args) {
--- exampletest/src/main/java/com/example/exampletest/service/TestService.java
+++ exampletest/src/main/java/com/example/exampletest/service/TestService.java
@@ -1,7 +1,6 @@
 package com.example.exampletest.service;
 
 import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +13,7 @@ public class TestService {
     public TestService() {
         System.out.println("TestService 생성자 호출");
         try {
+            System.out.println("testComponent : " + testComponent.getClass());
             testComponent.print();
         } catch (Exception e) {
             System.out.println("생성자 단계에서 호출 실패");
@@ -26,6 +26,7 @@ public class TestService {
         System.out.println("TestService @PostConstruct 호출");
 
         try {
+            System.out.println("testComponent : " + testComponent.getClass());
             testComponent.print();
         } catch (Exception e) {
             System.out.println("@PostConstruct 단계에서 호출 실패");

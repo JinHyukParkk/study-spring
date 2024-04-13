1d57110f0a12c13bab4104d341e2e0f2ee5ea544
hyuk <hyuk@woowahan.com>
2024-04-13 12:35:52 +0900

    세팅 업데이트

---
+++ simple-batch/src/main/java/com/example/simplebatch/SimpleBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBatchApplication.class, args);
+    }
+
+}
---
+++ simple-batch/src/test/java/com/example/simplebatch/SimpleBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
